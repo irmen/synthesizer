6e80a8a5390288a334b8c3fddb205ff934b81013
Irmen de Jong <irmen@razorvine.net>
2016-02-21 01:34:24 +0100

    synth tweaks

--- synth.py
+++ synth.py
@@ -36,48 +36,53 @@ class Wavesynth:
     Simple waveform sample synthesizer. Can generate various wave forms:
     sine, square (perfect or with harmonics), triangle and sawtooth.
     """
+    def __init__(self, samplerate=Sample.norm_samplerate, samplewidth=Sample.norm_sampwidth):
+        if samplewidth not in (1, 2, 4):
+            raise ValueError("only samplewidth sizes 1, 2 and 4 are supported")
+        self.samplerate = samplerate
+        self.samplewidth = samplewidth
 
-    def _get_array(self, samplewidth):
-        if samplewidth == 1:
+    def _get_array(self):
+        if self.samplewidth == 1:
             return array.array('b')
-        elif samplewidth == 2:
+        elif self.samplewidth == 2:
             return array.array('h')
-        elif samplewidth == 4:
+        elif self.samplewidth == 4:
             return array.array('l')
         else:
             raise ValueError("only samplewidth sizes 1, 2 and 4 are supported")
 
-    def _to_sample(self, samplearray, samplewidth, samplerate):
-        frames = samplearray.tobytes()
+    def to_sample(self, sample_array):
+        frames = sample_array.tobytes()
         if sys.byteorder == "big":
-            frames = audioop.byteswap(bytes, samplewidth)
-        return Sample.from_raw_frames(frames, samplewidth, samplerate, 1).fadeout(0.1)
+            frames = audioop.byteswap(bytes, self.samplewidth)
+        return Sample.from_raw_frames(frames, self.samplewidth, self.samplerate, 1).fadeout(0.1)
 
-    def sine(self, frequency, duration, samplerate=Sample.norm_samplerate, samplewidth=Sample.norm_sampwidth, amplitude=1.0):
-        samples = self._get_array(samplewidth)
-        scale = amplitude*(2**(samplewidth*8-1)-1)
-        rate = samplerate/frequency/2.0/pi
-        for t in range(int(duration*samplerate)):
+    def sine(self, frequency, duration, amplitude=1.0):
+        samples = self._get_array()
+        scale = amplitude*(2**(self.samplewidth*8-1)-1)
+        rate = self.samplerate/frequency/2.0/pi
+        for t in range(int(duration*self.samplerate)):
             samples.append(int(sin(t/rate)*scale))
-        return self._to_sample(samples, samplewidth, samplerate)
+        return samples
 
-    def square(self, frequency, duration, samplerate=Sample.norm_samplerate, samplewidth=Sample.norm_sampwidth, amplitude=1.0):
+    def square(self, frequency, duration, amplitude=1.0):
         """Generate a perfect square wave [max/-max]"""
-        samples = self._get_array(samplewidth)
-        scale = int(amplitude*(2**(samplewidth*8-1)-1))
-        rate = samplerate/frequency
-        for t in range(int(duration*samplerate)):
+        samples = self._get_array()
+        scale = int(amplitude*(2**(self.samplewidth*8-1)-1))
+        rate = self.samplerate/frequency
+        for t in range(int(duration*self.samplerate)):
             x = t/rate
             y = 2*floor(x)-floor(x*2)+1
             samples.append(y*scale)
-        return self._to_sample(samples, samplewidth, samplerate)
+        return samples
 
-    def squareh(self, frequency, duration, num_harmonics=10, samplerate=Sample.norm_samplerate, samplewidth=Sample.norm_sampwidth, amplitude=1.0):
+    def squareh(self, frequency, duration, num_harmonics=10, amplitude=1.0):
         """Generate a square wave based on harmonic sine waves (more natural)"""
-        samples = self._get_array(samplewidth)
-        scale = int(amplitude*(2**(samplewidth*8-1)-1))
-        f = frequency/samplerate
-        for t in range(int(duration*samplerate)):
+        samples = self._get_array()
+        scale = int(amplitude*(2**(self.samplewidth*8-1)-1))
+        f = frequency/self.samplerate
+        for t in range(int(duration*self.samplerate)):
             h = 0.0
             q = 2*pi*f*t
             for k in range(1, num_harmonics+1):
@@ -91,35 +96,35 @@ class Wavesynth:
             y = int(3.7/pi*h*scale)
             y = max(min(y, scale), -scale)
             samples.append(y)
-        return self._to_sample(samples, samplewidth, samplerate)
+        return samples
 
-    def triangle(self, frequency, duration, samplerate=Sample.norm_samplerate, samplewidth=Sample.norm_sampwidth, amplitude=1.0):
-        samples = self._get_array(samplewidth)
-        scale = amplitude*(2**(samplewidth*8-1)-1)
-        p = samplerate/frequency
-        for t in range(int(duration*samplerate)):
+    def triangle(self, frequency, duration, amplitude=1.0):
+        samples = self._get_array()
+        scale = amplitude*(2**(self.samplewidth*8-1)-1)
+        p = self.samplerate/frequency
+        for t in range(int(duration*self.samplerate)):
             y = 4*amplitude/p*(abs((t+p*0.75) % p - p/2)-p/4)
             samples.append(int(scale*y))
-        return self._to_sample(samples, samplewidth, samplerate)
+        return samples
 
-    def sawtooth(self, frequency, duration, samplerate=Sample.norm_samplerate, samplewidth=Sample.norm_sampwidth, amplitude=1.0):
-        samples = self._get_array(samplewidth)
-        scale = amplitude*(2**(samplewidth*8-1)-1)
-        a = samplerate/frequency
-        for t in range(int(duration*samplerate)):
+    def sawtooth(self, frequency, duration, amplitude=1.0):
+        samples = self._get_array()
+        scale = amplitude*(2**(self.samplewidth*8-1)-1)
+        a = self.samplerate/frequency
+        for t in range(int(duration*self.samplerate)):
             y = 2*(t/a - floor(0.5+t/a))
             samples.append(int(scale*y))
-        return self._to_sample(samples, samplewidth, samplerate)
+        return samples
 
-    def pulse(self, frequency, duration, width=500, samplerate=Sample.norm_samplerate, samplewidth=Sample.norm_sampwidth, amplitude=1.0):
+    def pulse(self, frequency, duration, width=500, amplitude=1.0):
         raise NotImplementedError  # XXX to be done
 
-    def white_noise(self, duration, samplerate=Sample.norm_samplerate, samplewidth=Sample.norm_sampwidth, amplitude=1.0):
-        samples = self._get_array(samplewidth)
-        scale = amplitude*(2**(samplewidth*8-1)-1)
-        for t in range(int(duration*samplerate)):
+    def white_noise(self, duration, amplitude=1.0):
+        samples = self._get_array()
+        scale = amplitude*(2**(self.samplewidth*8-1)-1)
+        for t in range(int(duration*self.samplerate)):
             samples.append(random.randint(-scale, scale))
-        return self._to_sample(samples, samplewidth, samplerate)
+        return samples
 
 
 def demo():
@@ -132,7 +137,7 @@ def demo():
         for note, freq in notes4.items():
             print("   {:f} hz".format(freq))
             sample = wave(freq, duration=0.2)
-            r.play_sample(sample)
+            r.play_sample(synth.to_sample(sample))
 
 
 def demo2():
@@ -143,9 +148,9 @@ def demo2():
     print("Synthesizing tones...")
     notes = {note: key_freq(49+i) for i, note in enumerate(['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'])}
     tempo = 0.2
-    quarter_notes = {note: synth.triangle(notes[note], tempo) for note in notes}
-    half_notes = {note: synth.triangle(notes[note], tempo*2) for note in notes}
-    full_notes = {note: synth.triangle(notes[note], tempo*4) for note in notes}
+    quarter_notes = {note: synth.to_sample(synth.triangle(notes[note], tempo)) for note in notes}
+    half_notes = {note: synth.to_sample(synth.triangle(notes[note], tempo*2)) for note in notes}
+    full_notes = {note: synth.to_sample(synth.triangle(notes[note], tempo*4)) for note in notes}
     song = "A A B. A D. C#.. ;  A A B. A E. D.. ;  A A A. F#.. D C#.. B ;  G G F#.. D E D ; ; ; ; "\
         "A A B. A D C#.. ; A A B. A E D. ; A A A. F#.. D C#.. B ; G G F#.. D E D ; ".split()
     for note in song:
