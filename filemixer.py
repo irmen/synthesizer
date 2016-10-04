9151fa89d9716c684dd08dedf8059830c30c3d99
Irmen de Jong <irmen@razorvine.net>
2016-10-04 03:11:12 +0200

    fixed file format probing

--- filemixer.py
+++ filemixer.py
@@ -4,22 +4,15 @@ from synthesizer.tools import AudiofileToWavStream, StreamMixer
 from synthesizer.sample import Output, Sample, LevelMeter
 
 
-def get_wav_stream(filename):
-    stream = AudiofileToWavStream(filename, samplerate=Sample.norm_samplerate,
-                                channels=Sample.norm_nchannels,
-                                sampleformat=str(8*Sample.norm_samplewidth))
-    return stream.convert()
-
-
 def main(args):
     if len(args) < 1:
         raise SystemExit("Mixes one or more audio files. Arguments: inputfile...")
-    wav_streams = [get_wav_stream(filename) for filename in args]
+    wav_streams = [AudiofileToWavStream(filename).convert() for filename in args]
     with StreamMixer(wav_streams, endless=True) as mixer:
         mixed_samples = iter(mixer)
         with Output(mixer.samplerate, mixer.samplewidth, mixer.nchannels) as output:
             levelmeter = LevelMeter(rms_mode=False, lowest=-50)
-            temp_stream = get_wav_stream("samples/909_crash.wav")
+            temp_stream = AudiofileToWavStream("samples/909_crash.wav").convert()
             for timestamp, sample in mixed_samples:
                 levelmeter.update(sample)
                 output.play_sample(sample)
