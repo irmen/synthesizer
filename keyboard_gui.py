8492e5ee931c204f36a79eaf45c81bbef89e4902
Irmen de Jong <irmen@razorvine.net>
2017-03-10 23:46:35 +0100

    keyboard shortcuts added to play notes

--- keyboard_gui.py
+++ keyboard_gui.py
@@ -264,9 +264,10 @@ class PianoKeyboardGUI(tk.Frame):
         black_key_height = white_key_height * 0.6
         num_octaves = 5
         first_octave = 2
-        canvas = tk.Canvas(self, width=white_key_width*num_octaves*7+2, height=white_key_height+1+20, borderwidth=0)
+        x_offset = 3
+        y_offset = 20
+        canvas = tk.Canvas(self, width=white_key_width*num_octaves*7+2, height=white_key_height+10+y_offset, borderwidth=0)
         # white keys:
-        xy_offset = 3
         for key_nr, key in enumerate("CDEFGAB"*num_octaves):
             octave = first_octave+key_nr//7
             def key_pressed(event, note=key, octave=octave):
@@ -275,10 +276,14 @@ class PianoKeyboardGUI(tk.Frame):
             def key_released(event, note=key, octave=octave):
                 gui.pressed(event, note, octave, True)
             x = key_nr * white_key_width
-            key_rect = canvas.create_rectangle(x+xy_offset, xy_offset, x+white_key_width+xy_offset, white_key_height+xy_offset, fill="white", outline="gray50", width=1, activewidth=2)
+            key_rect = canvas.create_rectangle(x+x_offset, y_offset, x+white_key_width+x_offset, white_key_height+y_offset, fill="white", outline="gray50", width=1, activewidth=2)
             canvas.tag_bind(key_rect, "<ButtonPress-1>", key_pressed)
             canvas.tag_bind(key_rect, "<ButtonRelease-1>", key_released)
-            canvas.create_text(x+white_key_width/2+2, white_key_height+xy_offset+1, text=key, anchor=tk.N, fill="gray")
+            canvas.create_text(x+white_key_width/2+2, 1, text=key, anchor=tk.N, fill="gray")
+            if 10 <= key_nr <= 21:
+                keychar = "qwertyuiop[]"[key_nr-10]
+                canvas.create_text(x+white_key_width/2+2, white_key_height+3, text=keychar, anchor=tk.N, fill="maroon")
+                gui.bind_keypress(keychar, key, octave)
         # black keys:
         for key_nr, key in enumerate((["C#", "D#", None, "F#", "G#", "A#", None]*num_octaves)[:-1]):
             if key:
@@ -289,7 +294,7 @@ class PianoKeyboardGUI(tk.Frame):
                 def key_released(event, note=key, octave=octave):
                     gui.pressed(event, note, octave, True)
                 x = key_nr * white_key_width + white_key_width*0.75
-                key_rect = canvas.create_rectangle(x+xy_offset, xy_offset, x+black_key_width+xy_offset, black_key_height+xy_offset, fill="black", outline="gray50", width=1, activewidth=2)
+                key_rect = canvas.create_rectangle(x+x_offset, y_offset, x+black_key_width+x_offset, black_key_height+y_offset, fill="black", outline="gray50", width=1, activewidth=2)
                 canvas.tag_bind(key_rect, "<ButtonPress-1>", key_pressed)
                 canvas.tag_bind(key_rect, "<ButtonRelease-1>", key_released)
         canvas.pack()
@@ -465,7 +470,7 @@ class SynthGUI(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master.title("Synthesizer")
-        self.keypress_sema = Semaphore(1)
+        self.keypress_sema = Semaphore(1)    # XXX improve mechanism, see keyboard presses?
         self.osc_frame = tk.Frame(self)
         self.oscillators = []
         self.piano_frame = tk.Frame(self)
@@ -532,6 +537,18 @@ class SynthGUI(tk.Frame):
         self.current_note = None
         self.echos_ending_time = 0
         self.arpeggio_playing = False
+        self.pressed_keyboard_keys = set()
+
+    def bind_keypress(self, key, note, octave):
+        def kbpress(event):
+            if key not in self.pressed_keyboard_keys:
+                self.pressed(event, note, octave, False)
+            self.pressed_keyboard_keys.add(key)
+        def kbrelease(event):
+            self.pressed(event, note, octave, True)
+            self.pressed_keyboard_keys.remove(key)
+        self.master.bind(key, kbpress)
+        self.master.bind("<KeyRelease-%s>" % key, kbrelease)
 
     def create_synth(self):
         samplerate = self.samplerate_choice.get()
