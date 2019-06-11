e5b9d095630aaf9599bafc03ecebc93b4933b83e
Irmen de Jong <irmen@razorvine.net>
2019-06-11 02:58:04 +0200

    added miniaudio decoders module

--- pyminiaudio/setup.py
+++ pyminiaudio/setup.py
@@ -1,11 +1,15 @@
 import os
+import glob
 from setuptools import setup, find_packages
 
 miniaudio_path = os.path.abspath(".")           # to make sure the compiler can find the required include files
 
 
-setup(name="pyminiaudio",
-      version="0.1",
+PKG_NAME = "pyminaudio"
+PKG_VERSION = "0.1"
+
+setup(name=PKG_NAME,
+      version=PKG_VERSION,
       description="miniaudio decoders (mp3, flac, ogg vorbis, wav) python bindings",
       author="Irmen de Jong",
       py_modules=["miniaudio_decoders"],
@@ -15,3 +19,6 @@ setup(name="pyminiaudio",
       cffi_modules=["./build_decoders_module.py:ffibuilder"],
       include_dirs=[miniaudio_path]
       )
+
+
+j
