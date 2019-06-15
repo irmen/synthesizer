5d46ba8db28ae6f9550b1ab9c0ffa2c12e19c3d9
Irmen de Jong <irmen@razorvine.net>
2019-06-15 15:11:56 +0200

    fix package version issue

--- pyminiaudio/setup.py
+++ pyminiaudio/setup.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 from setuptools import setup
 
 if sys.version_info < (3, 5):
@@ -7,8 +8,10 @@ if sys.version_info < (3, 5):
 
 
 miniaudio_path = os.path.abspath(".")  # to make sure the compiler can find the required include files
+PKG_VERSION = re.search(r'^__version__\s*=\s*"(.+)"', open("miniaudio.py", "rt").read(), re.MULTILINE).groups()[0]
 
 setup(
+      version=PKG_VERSION,
       cffi_modules=["./build_ffi_module.py:ffibuilder"],
       include_dirs=[miniaudio_path]
       )
