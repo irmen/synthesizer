c5a99eeac48123d5e14933b190d7ee06749e8253
Cameron Maske <cameronmaske@gmail.com>
2019-06-26 22:10:32 +0200

    Add types

--- pyminiaudio/examples/wire.py
+++ pyminiaudio/examples/wire.py
@@ -9,11 +9,11 @@ import numpy as np
 if __name__ == "__main__":
     in_device = miniaudio.CaptureDevice(buffersize_msec=0)
 
-    def callback(input_pointer, frame_count):
+    def callback(p_input: ffi.CData, frame_count: int):
         print("\n")
         print(frame_count)
         buffer = ffi.new("char *")
-        ffi.memmove(buffer, input_pointer, frame_count)
+        ffi.memmove(buffer, p_input, frame_count)
         as_bytes = ffi.string(buffer)
         print(len(as_bytes))
         data = np.frombuffer(as_bytes, dtype="int16")
