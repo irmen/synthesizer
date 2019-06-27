060282e0b0c09bcdb57012327e7c5fff4401f449
Cameron Maske <cameronmaske@gmail.com>
2019-06-27 10:13:56 +0200

    Copy pInput to a new byte array. Setup basic wire/duplex demo (buggy and noisy atm)

--- pyminiaudio/examples/wire.py
+++ pyminiaudio/examples/wire.py
@@ -8,18 +8,33 @@ import numpy as np
 
 if __name__ == "__main__":
     in_device = miniaudio.CaptureDevice(buffersize_msec=0)
+    out_device = miniaudio.PlaybackDevice(buffersize_msec=0)
 
-    def callback(p_input: ffi.CData, frame_count: int):
-        print("\n")
-        print(frame_count)
-        buffer = ffi.new("char *")
-        ffi.memmove(buffer, p_input, frame_count)
-        as_bytes = ffi.string(buffer)
-        print(len(as_bytes))
-        data = np.frombuffer(as_bytes, dtype="int16")
-        print(data)
+    buffer = b""
 
+    def in_callback(data: bytearray, frame_count: int):
+        global buffer
+        buffer += data
 
-    in_device.start(callback)
-    sleep(1)
+    def out_callback() -> miniaudio.AudioProducerType:
+        required_frames = yield b""  # generator initialization
+        global buffer
+        while True:
+            required_bytes = required_frames * out_device.nchannels * out_device.sample_width
+            if required_bytes > len(buffer):
+                data = ""
+            else:
+                data = buffer[0: required_bytes]
+                buffer = buffer[required_bytes: ]
+            print(".", end="", flush=True)
+            required_frames = yield data
+
+    in_device.start(in_callback)
+    gen = out_callback()
+    gen.send(None)
+    out_device.start(gen)
+
+    sleep(5)
+
     in_device.stop()
+    out_device.stop()
