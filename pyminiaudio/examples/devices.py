4e0278357fca095f3c68e064c134387eb9074b59
Cameron Maske <cameronmaske@gmail.com>
2019-06-30 11:08:05 +0200

    Add ability to list playback and captures devices. Add ability to select device when starting a PlaybackDevice stream

---
+++ pyminiaudio/examples/devices.py
@@ -0,0 +1,23 @@
+import os
+import miniaudio
+import json
+
+if __name__ == "__main__":
+    devices = miniaudio.Devices()
+    print("Backend: {}".format(devices.backend))
+    print("\n")
+
+    out_devices = devices.get_playbacks()
+    print("Playback Devices")
+    for device in out_devices:
+        print("  {}".format(device.name))
+        print("    {}".format(device.info()))
+        print("\n")
+
+    in_devices = devices.get_captures()
+    print("Capture Devices")
+    for device in in_devices:
+        print("  {}".format(device.name))
+        print("    {}".format(device.info()))
+        print("\n")
+
