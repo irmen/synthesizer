4f702229252fec065adae3e6ecbcccbe76affca6
Irmen de Jong <irmen@razorvine.net>
2019-07-03 17:00:12 +0200

    debug print

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup, find_packages
 if sys.version_info < (3, 5):
     raise SystemExit("Synthplayer requires Python 3.5 or newer")
 
-p = find_packages()
-print(p)
-
 setup(
     packages=find_packages()
 )
